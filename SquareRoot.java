55267e24ed01621986e7cf1e9ad8bc0b877a0c61
Niranjan Rathor <84126253+NiranjanRathor47@users.noreply.github.com>
2022-02-18 02:03:40 +0530

    Delete SquareRoot.java

--- SquareRoot.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class SquareRoot {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		System.out.print((int)Math.sqrt(n));
-		
-	}
-}
